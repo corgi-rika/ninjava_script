9c22035def3e4993ccf5b8609adbbc7ab06ef83d
corgi-rika <scaricky3132@gmail.com>
2024-08-15 00:46:34 +0900

    usersrテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_13_153904) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_14_154334) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "first_name", null: false
