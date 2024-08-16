b1b6a81ffedddb77bef69f3b194d5e5d206c8fa4
corgi-rika <scaricky3132@gmail.com>
2024-08-16 22:27:08 +0900

    単語編集機能の実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,6 @@
 class WordsController < ApplicationController
   before_action :set_user
-  before_action :set_word, only: [:show, :destroy]
+  before_action :set_word, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -20,9 +20,20 @@ class WordsController < ApplicationController
   def create
     @word = @user.words.build(word_params)
     if @word.save
-      redirect_to root_path, notice: '単語が登録されました。'
+      redirect_to user_words_path(@user), notice: '単語が登録されました。'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @word.update(word_params)
+      redirect_to user_word_path(@user, @word)
+    # else
+    #   render :edit, status: :unprocessable_entity
     end
   end
 
@@ -38,7 +49,7 @@ class WordsController < ApplicationController
   end
 
   def set_word
-    @word = @user.words.find_by(params[:id])
+    @word = @user.words.find_by(id: params[:id])
     # 削除済みの単語の詳細ページにアクセスしようとした場合にトップページにリダイレクト
     if @word.nil?
       redirect_to root_path, alert: '指定された単語は存在しません。'
