fe6ebba55b10243ce93dc85bc47233651a45e1fb
corgi-rika <scaricky3132@gmail.com>
2024-08-27 17:40:05 +0900

    アソシエーションの設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ApplicationRecord
   belongs_to :user
-  belongs_to :mentor
+  belongs_to :mentor, class_name: 'User', optional: true  # MentorはUserを指すと定義
+  belongs_to :user  # ユーザーに紐付けられた日報
 end
