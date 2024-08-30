4e7ada9fa1912b80b6cd4d6ecef4d644a13fb3a5
corgi-rika <scaricky3132@gmail.com>
2024-08-30 13:08:27 +0900

    Reportモデルのアソシエーション設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,12 @@
 class Report < ApplicationRecord
-  belongs_to :user
-  belongs_to :mentor, class_name: 'User', optional: true  # MentorはUserを指すと定義
+  belongs_to :mentor, class_name: 'User', foreign_key: 'mentor_id', optional: true  # 任意で紐付けられるメンター
   belongs_to :user  # ユーザーに紐付けられた日報
+
+  with_options presence: true do
+    validates :total_study_time
+    validates :improvement_points
+    validates :next_steps
+    validates :next_study_day
+  end
+
 end
