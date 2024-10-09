7b7c9c7ee1472527df4ece4e59b2c5a744c9aaa9
corgi-rika <scaricky3132@gmail.com>
2024-10-09 23:29:56 +0900

    正解/不正解アイコンの位置の調整

--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -25,14 +25,12 @@ document.addEventListener('turbo:load', function() {
         if (userAnswer === correctAnswer) {
           quizModalMessage.textContent = 'すばらしい！正解です！';
           quizModal.classList.add('correct');  // quizModalに正解のクラスを追加
-          quizModalIcon.src = '/assets/badge-check.svg'; // 正解のアイコンを設定
-          quizModalIcon.classList.add('correct');  // 正解のアイコンに正しいクラスを追加
+          quizModalIcon.classList.add('correct');  // アイコンの正解用クラスを追加
           correctSound.play(); // 正解時の音声を再生
         } else {
           quizModalMessage.textContent = 'ざんねん！不正解です。';
           quizModal.classList.add('incorrect');  // quizModalに不正解のクラスを追加
-          quizModalIcon.src = '/assets/badge-check.svg';  // 不正解でも同じアイコンを使う場合
-          quizModalIcon.classList.add('incorrect');  // 不正解のアイコンにクラスを追加
+          quizModalIcon.classList.add('incorrect');  // アイコンの不正解用クラスを追加
           incorrectSound.play(); // 不正解時の音声を再生
         }
 
